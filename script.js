25887210a69b3bcb50942615b16d50425c055f3e
TheDanishMexican <danieljchristjansen@gmail.com>
2023-04-26 17:10:39 +0200

    closeDialogDelete and styling
    Added function to close the delete dialog when cancel is clicked, also added necessary event listeners. Also did some styling on buttons in the delete dialog.

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ window.addEventListener("load", main);
 const endpoint =
   "https://gallopgalore-80085-default-rtdb.europe-west1.firebasedatabase.app/";
 
-function main() {}
+function main() {
+  /* ========== EVENT LISTENERS ========== */
+  //I am aware that these probably need to be placed elsewhere,
+  //but I don't know where would be the best, so they are just here for now
+  const cancelButtonInDialogDelete = document.querySelector(
+    "#cancelButtonInDialogDelete"
+  );
+  cancelButtonInDialogDelete.addEventListener("click", closeDialogDelete);
+}
 
 /* ========== UPDATE ========== */
 // Sends put request to endpoint with horse object
@@ -49,4 +57,13 @@ async function deleteHorse(horseID) {
     deleteDialog.close();
     updateGrid();
   }
+}
+
+/* ========== DELETE ========== */
+//This function closes the delete dialog when the cancel button is clicked.
+//This function is called in the main function by an evenlistener.
+
+function closeDialogDelete() {
+  const dialogDelete = document.querySelector("#dialogDelete");
+  dialogDelete.close();
 }
